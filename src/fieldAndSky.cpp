fa6d5b4ea1a6b3e4dfedafeb7c22f6dc966f539f
Rustyard <1092882207@qq.com>
2021-06-02 20:48:11 +0800

    disable cull face

--- src/fieldAndSky.cpp
+++ src/fieldAndSky.cpp
@@ -537,7 +537,7 @@ void setup()
 
     glClearColor(1.0, 1.0, 1.0, 0.0);
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
+    // glEnable(GL_CULL_FACE);
     glEnable(GL_DEPTH_CLAMP);
 
     // load obj models
